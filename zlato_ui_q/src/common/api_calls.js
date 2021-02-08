cde5e4d8565dc6de0ef26168560d549cf23b2f7b
Vadecha <anuj.vadecha@ip-192-168-0-100.ap-south-1.compute.internal>
2021-02-08 17:45:18 +0530

    http->https

--- zlato_ui_q/src/common/api_calls.js
+++ zlato_ui_q/src/common/api_calls.js
@@ -1,5 +1,5 @@
 import { apiService } from './api.service'
-const base_url = 'http://admin.deltabx.com'
+const base_url = 'https://admin.deltabx.com'
 const base_websocket_url = 'admin.deltabx.com'
 // const base_url = 'http://localhost:8000'
 // const base_websocket_url = 'localhost:8000'
