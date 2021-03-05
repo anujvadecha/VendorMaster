92e7770d8b00a4cf5a22ff9f06b915b1edfbd0ae
Vadecha <anuj.vadecha@ip-192-168-0-100.ap-south-1.compute.internal>
2021-03-06 00:47:31 +0530

    casd

--- vendorbase/admin.py
+++ vendorbase/admin.py
@@ -285,9 +285,8 @@ class VendorMargin(admin.ModelAdmin):
     list_editable = ('margin',)
     list_display_links = ('user',)
     search_fields = ('user')
-    exclude = ('avg_rating','no_of_ratings')
     def get_readonly_fields(self, request, obj=None):
         if(request.user.is_superuser):
             return ()
         else:
-            return ('user', 'vendor', 'margin_available')
+            return ('user', 'vendor', 'margin_available','avg_rating','no_of_ratings')
